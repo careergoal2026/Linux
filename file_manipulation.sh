8a684db65b13746e64a514f3035138bf3201220a
careergoal2026 <careergoal2026@gmail.com>
2025-12-30 12:38:45 +0000

    Copy, Rename, and Delete Files

---
+++ file_manipulation.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "You must begin inside the directory(/home/user/practice) where your three original files are located.
+ From there, you will create a dedicated folder named "backup" to store a backup copy of one file.
+ If this folder does not already exist, you need to create it.
+ File "a.txt" must be copied into this folder, meaning the original remains in place while a
+ duplicate is stored separately. Another file, "b.txt" needs to be renamed to "b_renamed.txt"
+ this operation modifies only the name and keeps the file in the same location.
+ Lastly, file "c.txt" must be deleted, meaning it should no longer appear in the directory when you list the items.
+
+Ensure the backup folder exists
+
+Ensure the first file was correctly copied into it
+
+Ensure the second file was renamed correctly
+
+Ensure the third file was deleted entirely"
+cd practice/
+pwd
+sudo mkdir backup
+ls
+sudo cp a.txt backup/a.txt
+sudo mv b.txt b_renamed.txt
+sudo rm c.txt
+ls 
