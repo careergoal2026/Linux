1092fb1616b3bff06811c42e30fc842792413a20
careergoal2026 <careergoal2026@gmail.com>
2026-01-06 10:07:05 +0000

    Exploring Navigation and File Listing

---
+++ navigation.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Display the absolute path of the current working directory.
+
+List the files and directories in the current directory.
+
+Navigate into the directory named task_8, which is located in the current directory.
+
+Return to your home directory using the appropriate flag with the cd command.
+
+Navigate back to the previous directory using the correct shortcut.
+
+Use the cd command to move to the parent directory of your current location.
+
+"
+pwd 
+ls
+cd task_8
+cd -
+cd ..
+ 
