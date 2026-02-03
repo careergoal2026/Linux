8cec5c9f29b9286182096c67e4d7326b590fbc9a
careergoal2026 <careergoal2026@gmail.com>
2026-02-03 10:50:20 +0000

    Linux day 1 q-1

---
+++ Linux_day_1.1.sh
@@ -0,0 +1,45 @@
+echo "In this assignment, we will practice some basic Linux commands. Below are a few tasks. You need to store your answer in an answer.txt file using the command given for each task. Lastly, test your answer by clicking the submit button.
+
+NOTE - In the question, we are using the echo command. It is commonly used in shell scripts and command-line operations to output text to the terminal or write text to a file.
+
+
+TASK - 1:
+
+Let us start with a simple command to know the user's current location. Run the command in the terminal and store the output returned on the terminal in the answer.txt file using the below command:
+
+echo output >> /home/user/answer.txt
+
+Replace output with the value to be stored in answer.txt. For example, if the value to be stored is sudo, then the above command will look like this:
+
+echo sudo >> /home/user/answer.txt
+
+NOTE: For tasks 2 and 3 storing only the command name will pass the test cases, make sure nothing else is stored in the answer.txt file.
+
+TASK - 2:
+
+A directory named task_1 has been created in your current location. Use a Linux command to change directory to the task_1 directory.
+
+Store the command used to move to the task_1 directory in answer.txt using the below command:
+
+echo command_name >> /home/user/answer.txt
+
+TASK - 3:
+
+We have created some directories in the task_1 directory. Use a Linux command to list all the directories present in it.
+
+Store the command used to list files and directories in the task_1 directory in answer.txt using the below command:
+
+echo command_name >> /home/user/answer.txt
+
+TASK - 4:
+
+We have stored a hidden file in the task_1 directory. Use a Linux command to list all the files in a directory and hidden files using arguments or flags with the ls command.
+
+Store the name of the hidden file in the task_1 directory in answer.txt using the below command:
+
+echo file_name >> /home/user/answer.txt"
+echo "pwd" >>answer.txt
+echo "cd task_1" >>answer.txt
+echo "ls">>answer.txt
+echo ".git" >>answer.txt
+
