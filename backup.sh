e9d8cc9473979ba1f0eeaef6195062d9884f06d6
careergoal2026 <careergoal2026@gmail.com>
2026-02-24 09:11:39 +0000

    24 feb

--- backup.sh
+++ backup.sh
@@ -12,4 +12,14 @@ if [ ! -d "$BACKUP_DIR" ];then
   fi
 fi
 
- 
+echo "#Check if enough space is there for backup dir or not, if not, exit the script"
+
+REQUIRED_SPACE_IN_MB=10
+AVAILABLE_SPACE=$(df -m "$BACKUP_DIR" | awk 'NR==2 {print $4}')
+if [ "$AVAILABLE_SPACE" -lt "$REQUIRED_SPACE_IN_MB" ];then
+  echo "Not enought space. Required space is : {$REQUIRED_SPACE_IN_MB} Available space is :{$AVAILABLE_SPACE}"
+  exit 1
+echo "Enough space available"
+fi
+
+
