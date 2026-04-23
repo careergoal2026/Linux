ed35cd1887b7aa61f10cbf388200180224d701e7
root <careergoal2026>
2026-04-23 18:59:32 +0000

    24 aprl

--- memory_leak.sh
+++ memory_leak.sh
@@ -3,26 +3,24 @@
 p_name=$1
 threshold=0.1
 
-PIDS=$(ps -C "$p_name" -o pid)
+PIDS=$(ps -C "$p_name" -o pid=)
 mkdir -p snapshot
-touch snapshot/ps.txt
-touch snapshot/top.txt
-touch snapshot/proc.txt
+ touch snapshot/ps.txt
 
-ts=$(date +%H%M%S)
 for pid in $PIDS
 do
   mem_pct=$(ps -p "$pid" -o %mem= | tr -d ' ')
   echo "$mem_pct"
-  if [[ $mem_pct > $threshold ]]
+  if (( $(echo "$mem_pct > $threshold" | bc -l) ))
   then
+    ts=$(date +%Y-%m-%d_%H%M%S)
     echo "Threshold exceeds for pid : $pid"
-    echo "timestamp :$ts">snapshot/ps.txt
-    echo "-------------------------------PS snapshot---------------------------"
-    ps -p "$pid" -o %mem= | tr -d ' '>> snapshot/ps.txt
-    echo "-------------------------------TOP snapshot---------------------------"
-    top -b -n 1 -p "$pid">>snapshot/top.txt
-    echo "-------------------------------PROC snapshot---------------------------"
-     cat proc/$pid/status >> snapshot/proc.txt
+    echo "timestamp :$ts">>snapshot/ps.txt
+    echo "-------------------------------PS snapshot---------------------------">>snapshot/ps.txt
+    ps -p "$pid" -o %mem,cmd,pid | tr -d ' '>> snapshot/ps.txt
+    echo "-------------------------------TOP snapshot---------------------------">>snapshot/ps.txt
+    top -b -n 1 -p "$pid">>snapshot/ps.txt
+    echo "-------------------------------PROC snapshot---------------------------">>snapshot/ps.txt
+     cat /proc/$pid/status >> snapshot/ps.txt
   fi
 done
